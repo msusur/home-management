476c23305cd8bc67857390661612e9c61d72d86d
msusur <mail@mertsusur.com>
2013-10-29 10:33:24 +0200

    new routing fixes. todo view added.

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,6 +2,15 @@ module.exports.routes = {
 
   '/': {
     view: 'home/index'
+  },
+  'post /todo/create' : {
+    controller: 'Todo',
+    action:'create'
+  },
+  '/todo/all':{
+    view: 'todo/all'
+  },
+  '/notes/all':{
+    view: 'notes/all'
   }
-
 };
